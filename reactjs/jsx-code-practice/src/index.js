24e5758a59faed5c73bb4c2927961a96a36140f4
Sakshi Shinde <sakshi@Sakshis-MacBook-Air-7.local>
2025-01-27 13:43:52 +0530

    JSX done

---
+++ reactjs/jsx-code-practice/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div>
+        <h1>My fav food</h1>
+        <ul>
+            <li>Biryani</li>
+            <li>Fried chicken</li>
+            <li>Cake</li>
+        </ul>
+    </div>,
+    document.getElementById("root")
+);
