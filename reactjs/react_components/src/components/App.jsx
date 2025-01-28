fe48d93a1c0eaff7d385048e2831ffc865fa9d31
Sakshi Shinde <sakshi@Sakshis-MacBook-Air-8.local>
2025-01-28 23:24:18 +0530

    react components done

---
+++ reactjs/react_components/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import List from "./List";
+
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <List />
+        </div>
+    );
+}
+
+export default App;
