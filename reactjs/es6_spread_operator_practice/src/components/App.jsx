8fe5c802e32f17ba7022ed98c2b3a675054e7ba2
Sakshi Shinde <sakshi@Sakshis-MacBook-Air-8.local>
2025-02-04 19:50:30 +0530

    spread operator done

---
+++ reactjs/es6_spread_operator_practice/src/components/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function handleClick() {
+    setItems((prevValue) => {
+      return[
+        ...prevValue,
+        inputText
+      ]
+    });
+    setInputText("");
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText}/>
+        <button onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map(todoItem => <li>{todoItem}</li>)}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
